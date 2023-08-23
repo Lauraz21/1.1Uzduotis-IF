acc81e5ed318c4406ad8979bc11be7014deebc51
Laura <Laura@DESKTOP-BESSB6E>
2023-08-23 22:31:06 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Iveskite dienos numeri: ");
+string day = Console.ReadLine();
+if (day == "1")
+{
+    Console.WriteLine("Pirmadienis");
+}
+else if (day == "2")
+{
+    Console.WriteLine("Antradienis");
+}
+else if (day == "3")
+{
+    Console.WriteLine("Treciadienis");
+}
+else if (day == "4")
+{
+    Console.WriteLine("Ketvirtadienis");
+}
+else if (day == "5")
+{
+    Console.WriteLine("Penktadienis");
+}
+else if (day == "6")
+{
+    Console.WriteLine("Sestadienis");
+}
+else if (day == "7")
+{
+    Console.WriteLine("Sekmadienis");
+}
+else
+{
+    Console.WriteLine("Neteisingas dienos numeris");
+}
